9b7cb55878876cbd3963ec73d12f205e421dd994
st146200 <No.003@mail.ru>
2025-10-06 11:39:14 +0000

    Cto-to staralsya

--- .ipynb_checkpoints/Life_expectancy-checkpoint.py
+++ .ipynb_checkpoints/Life_expectancy-checkpoint.py
@@ -4,9 +4,6 @@ avg_age_rus = 74
 avg_age_hk = 85
 days_left = avg_age_rus * 365 - age * 365
 days_diff = (avg_age_hk * 365) - (avg_age_rus * 365)
-days_happy = (days_left * 0.1) 
 print(f"Average life expectancy in Russia is about {avg_age_rus}")
 print(f"In average you still have to live for about {days_left} days more. Bruh...")
 print(f"Average random man from Hong Kong will live {days_diff} days more.")
-print(f"There only {days_happy} happy days left")
-print(f"Good luck")
